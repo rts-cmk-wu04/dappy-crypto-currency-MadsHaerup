a6cc4f0531ec1a965a8d40e3116c3274dd911a80
Mads Haerup <506094@rts-365.dk>
2021-03-23 11:21:59 +0100

    background and variables

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
-import "./App.css";
-
+import "./App.scss";
+import cloud from "./img/cloud.svg";
+import bottomRightWave from "./img/bottomwave.svg";
+import bottomLeftWave from "./img/bottomwave2.svg";
 function App() {
-	return <div className="App"></div>;
+	return (
+		<div className="app">
+			<div className="app__leftCornerCloud">
+				<img src={cloud} alt="green cloud" />
+			</div>
+
+			<div className="app__rightCornerCloud">
+				<img className="app__rightCornerCloud__left" src={bottomLeftWave} alt="wave" />
+				<img className="app__rightCornerCloud__right" src={bottomRightWave} alt="wave" />
+			</div>
+		</div>
+	);
 }
 
 export default App;
